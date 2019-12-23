fde9b13293eed7d76e7b1a1dd5d2c41f083a28ce
Stephen Francis <stephen.d.francis@gmail.com>
2019-12-23 07:42:21 +0000

    move most code out to dependencies

--- src/drawing/console.ts
+++ src/drawing/console.ts
@@ -1,47 +1,40 @@
 
-import BellmanFord from "../layout/BellmanFord";
-import Diagram from "../core/Diagram";
-import FileOutput from "../drawing/FileOutput";
-import FinishConnectors from "../layout/FinishConnectors";
-import Lee from "../layout/Lee";
-import MapLoaderFile from "../loaders/MapLoaderFile";
-import OverlapFixer from "../layout/OverlapFixer";
-import Scale from "../layout/Scale";
+import * as Diagram from "diagram-api";
+import * as SVG from "svg-api";
 
 
-const diagram = new Diagram();
-const loader = new MapLoaderFile(diagram);
+const domain = new Diagram.Domain();
+const loader = new Diagram.MapLoader(domain);
 loader.readFile("public/mars.md");
 
-const bf: BellmanFord = new BellmanFord();
-bf.beginDiagram(diagram);
+const bf: Diagram.BellmanFord = new Diagram.BellmanFord();
+bf.beginDomain(domain);
 while (bf.iterate());
 
-const of: OverlapFixer = new OverlapFixer();
-of.beginDiagram(diagram);
+const of: Diagram.OverlapFixer = new Diagram.OverlapFixer();
+of.beginDomain(domain);
 while (of.iterate());
 
-const sc: Scale = new Scale("double_cell");
-sc.beginDiagram(diagram);
+const sc: Diagram.Scale = new Diagram.Scale("double_cell");
+sc.beginDomain(domain);
 while (sc.iterate());
 
-console.log(diagram.output());
+console.log(domain.output());
 
-const le: Lee = new Lee();
-le.beginDiagram(diagram);
+const le: Diagram.Lee = new Diagram.Lee();
+le.beginDomain(domain);
 while (le.iterate());
 
-const s2: Scale = new Scale("svg");
-s2.beginDiagram(diagram);
+const s2: Diagram.Scale = new Diagram.Scale("svg");
+s2.beginDomain(domain);
 while (s2.iterate());
 
-const fc: FinishConnectors = new FinishConnectors();
-fc.layoutDiagram(diagram);
+const fc: Diagram.FinishConnectors = new Diagram.FinishConnectors();
+fc.layoutDomain(domain);
 
 // console.log(le.output("lines_vert"));
 
 // console.log(diagram.output());
 
-const fo: FileOutput = new FileOutput(diagram);
-fo.write("./dist/lee.html");
-
+const fm = new SVG.FileManager("./dist");
+fm.saveAsSVG(domain.draw(), "lee");
